d89d6a04e64a16bfb6e78f8b1d3b6138b70cc677
Kawatwist <masse.loic2@hotmail.com>
2019-05-12 17:07:33 +0200

    inv

--- src/game/hud.c
+++ src/game/hud.c
@@ -6,7 +6,7 @@
 /*   By: lomasse <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/10 10:06:33 by lomasse           #+#    #+#             */
-/*   Updated: 2019/05/10 12:43:14 by lomasse          ###   ########.fr       */
+/*   Updated: 2019/05/12 15:21:23 by lomasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,5 @@ void		display_crosshair(t_win *wn)
 	SDL_SetRenderDrawColor(wn->rend, 0, 0, 0, 0);
 	SDL_RenderDrawLine(wn->rend, (XSCREEN / 2) - 5, YSCREEN/2, (XSCREEN / 2) + 5, YSCREEN/2);
 	SDL_RenderDrawLine(wn->rend, XSCREEN/2, (YSCREEN / 2) - 5, XSCREEN/2, (YSCREEN / 2) + 5);
+	SDL_RenderCopy(wn->rend, ((t_text *)wn->player->weapon->text)->txt, NULL, NULL);
 }
--- src/init/init.c
+++ src/init/init.c
@@ -6,7 +6,7 @@
 /*   By: lomasse <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/07 16:00:01 by lomasse           #+#    #+#             */
-/*   Updated: 2019/05/10 09:48:14 by lomasse          ###   ########.fr       */
+/*   Updated: 2019/05/12 15:16:42 by lomasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int				init(t_win **wn, int argc, char **argv)
 	initwn(wn);
 	initsdl(wn);
 	init_input(wn);
-	initplayer(wn);
 	(*wn)->loading = initload2(wn, "./texture/loading.tga");
 	(*wn)->loadingscreen = initload2(wn, "./texture/loadingscreen.tga");
 	showload(wn, 10);
@@ -28,6 +27,9 @@ int				init(t_win **wn, int argc, char **argv)
 	parsearg(argc, argv, wn) == 0 ? stop_exec("Parsing error\n", *wn) : 0;
 	showload(wn, 30);
 	(*wn)->quality == 0 ? loadnothread(wn) : loadminimenu(wn);
+	(*wn)->load = ft_strdup("./texture/game/hand.tga");
+	load_texture(*wn, "game", "hand", "none");
+	initplayer(wn);
 	initskybox(wn);
 	return (1);
 }
