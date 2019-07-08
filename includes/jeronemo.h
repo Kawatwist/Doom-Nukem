20df1cd24d86e3a10791035ef5542c73180668a5
jchardin <jerome.chardin@outook.com>
2019-07-08 12:53:23 +0200

    ca tourne

--- includes/jeronemo.h
+++ includes/jeronemo.h
@@ -6,7 +6,7 @@
 /*   By: jchardin <jerome.chardin@outlook.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/08 16:37:05 by jchardin          #+#    #+#             */
-/*   Updated: 2019/07/08 12:43:19 by jchardin         ###   ########.fr       */
+/*   Updated: 2019/07/08 12:52:32 by jchardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -279,28 +279,46 @@ typedef struct			s_mykeep
 
 
 
-/**
- ** DIR WINDOW
- **/
 void		ft_launch_window(t_mywin *s_win, t_interface interface, t_win *wn);
 void		ft_clear_window(t_mywin *s_win);
 void		ft_draw_line(t_mywin *s_win, t_myputtheline *s_line);
+t_mycolor	ft_setcolor(int rrr, int ggg, int bbb);
+void		ft_quit(char *txt, t_mywin *s_win);
+void		ft_launch_rasterization(t_mywin *s_win, t_win *wn);
+void		ft_launch_bsp_tree(t_mywin *s_win, t_win *wn);
 
-/**
- ** BSP Tree generator functions definitions
- **/
+float		ft_dot_product(t_myvec v1, t_myvec v2);
+t_myvec		ft_cross_product(t_myvec v1, t_myvec v2);
+t_myvec		ft_vector_from_two_points(t_myvec v2, t_myvec v1);
+int			ft_abs(int number);
+t_myvec		ft_calculate_normal_of_points(t_myvec vertex1, t_myvec vertex2, t_myvec vertex3);
+float		ft_atoi_comma(const char *str);
+
+int			ft_get_nbr_of_triangle(t_mywin *s_win);
+t_mytriangle *ft_get_triangles_array(t_mywin *s_win);
+void		ft_draw_triangle_base(t_myvec *v1, t_myvec *v2, t_myvec *v3, t_mywin *s_win);
 
 
-/**
- ** Share !!
- **/
+void			ft_display_the_polygon_list(t_mypolygon *polygon_lst);
+t_mypolygon		*ft_read_the_polygon_file(void);
+void			ft_process_polygon(t_mypolygon *polygon_lst);
+int				ft_classify_polygon(t_mypolygon *plane, t_mypolygon *polygon_node);
+int				ft_classify_point(t_myvec point, t_mypolygon *plane);
+void			ft_split_polygon(t_mypolygon *poly,
+							t_mypolygon *plane,
+							t_mypolygon *front_split,
+							t_mypolygon *back_split);
+void			ft_build_bsp_tree(t_mynode *current_node, t_mypolygon *polygon_lst);
+t_mypolygon		*ft_select_the_best_poly_splitter(t_mypolygon *polygon_lst);
+void			ft_split_polygon(t_mypolygon *poly,
+							t_mypolygon *plane,
+							t_mypolygon *front_split,
+							t_mypolygon *back_split);
+void			ft_add_polygon(t_mypolygon **polygon_lst, t_mypolygon *polygon_node);
+void			ft_afficher_le_bsp(t_mynode *s_node);
 
-/**
- ** Map editon functions definitions
- **/
 /* void			ft_draw_square(t_mywin *s_win, t_mysquare *s_square); */
 /* t_mysquare	ft_setsquare(int x, int  y, int  width, int  height, t_mycolor s_color ); */
-t_mycolor	ft_setcolor(int rrr, int ggg, int bbb);
 /* void			ft_draw_cross(t_mywin *s_win, t_mycross *s_cross); */
 /* t_mycross	ft_setcross(int x, int y, int size, int thickness, t_mycolor color); */
 /* t_mywrite	ft_setwrite(int x, int y, t_mycolor color, int size, char *str); */
@@ -312,7 +330,6 @@ t_mycolor	ft_setcolor(int rrr, int ggg, int bbb);
 /* void			ft_draw_wall(t_mywin *s_win, t_mywall *s_wall); */
 /* void			ft_display_ihc(t_mywin *s_win); */
 /* t_mywall		*ft_read_map(void); */
-void			ft_quit(char *txt, t_mywin *s_win);
 /* void			ft_save_map(t_mywin *s_win); */
 /* t_mywall		*ft_create_wall_node(t_mywall wall); */
 /* void			ft_add_wall_node(t_mywall **lst_wall, t_mywall *s_wall); */
@@ -327,92 +344,34 @@ void			ft_quit(char *txt, t_mywin *s_win);
 /* void			ft_update_show_cross(t_mywin *s_win, t_win *wn); */
 /* void			ft_init_show_cross(t_mywin *s_win); */
 /* void			ft_launch_map_editor(t_mywin *s_win, t_win *wn); */
-void			ft_launch_rasterization(t_mywin *s_win, t_win *wn);
-void			ft_launch_bsp_tree(t_mywin *s_win, t_win *wn);
 /* void			ft_display_right_pan(t_mywin *s_win); */
-//bsp function
-
-
-
-//maths function
-float			ft_dot_product(t_myvec v1, t_myvec v2);
-t_myvec			ft_cross_product(t_myvec v1, t_myvec v2);
-t_myvec			ft_vector_from_two_points(t_myvec v2, t_myvec v1);
-int				ft_abs(int number);
-t_myvec			ft_calculate_normal_of_points(t_myvec vertex1, t_myvec vertex2, t_myvec vertex3);
-float			ft_atoi_comma(const char *str);
-
-
-
-
-
-void			ft_display_the_polygon_list(t_mypolygon *polygon_lst);
-t_mypolygon		*ft_read_the_polygon_file(void);
-void			ft_process_polygon(t_mypolygon *polygon_lst);
-int				ft_classify_polygon(t_mypolygon *plane, t_mypolygon *polygon_node);
-int				ft_classify_point(t_myvec point, t_mypolygon *plane);
-void			ft_split_polygon(t_mypolygon *poly,
-							t_mypolygon *plane,
-							t_mypolygon *front_split,
-							t_mypolygon *back_split);
-
-void			ft_build_bsp_tree(t_mynode *current_node, t_mypolygon *polygon_lst);
-t_mypolygon		*ft_select_the_best_poly_splitter(t_mypolygon *polygon_lst);
-void			ft_split_polygon(t_mypolygon *poly,
-							t_mypolygon *plane,
-							t_mypolygon *front_split,
-							t_mypolygon *back_split);
-
-void			ft_add_polygon(t_mypolygon **polygon_lst, t_mypolygon *polygon_node);
-void			ft_afficher_le_bsp(t_mynode *s_node);
-
-t_myvec	ft_rotation_x(float angle, t_myvec vertex);
-t_myvec	ft_rotation_y(float angle, t_myvec vertex);
-t_myvec	ft_rotation_z(float angle, t_myvec vertex);
-t_myvec	ft_matrix_multiply(float matrix[5][5], t_myvec vertex);
-t_myvec	ft_translation_x(float value, t_myvec vertex);
-t_myvec	ft_translation_y(float value, t_myvec vertex);
-t_myvec	ft_translation_z(float value, t_myvec vertex);
-t_myvec	ft_scale(float zoom, t_myvec vertex);
-/* void	ft_draw_change(t_mywin *s_win, t_mychange *change); */
-t_myvec		ft_normalise(t_myvec vector);
-//void		ft_mouse_event_check(t_win *wn, t_mychange *change, t_myraster *raster);
-void	ft_display_panel();
-void	ft_draw_triangle_base(t_myvec *v1, t_myvec *v2, t_myvec *v3, t_mywin *s_win);
-
-
-float	**ft_make_projection(void);   //DONE
-float	**ft_make_rotation_x(float theta);  //DONE
-float	**ft_make_rotation_z(float theta); //DONE
-
-float	**ft_matrix_multiply_matrix(float **m1, float **m2);  //surement DONE
-float	**ft_make_matrix_5_5(void);
-float	**ft_make_translation(float x, float y, float z);
-
-
-
-t_myvec	ft_perspective_projection(t_myvec vertex);
-
-int				ft_get_nbr_of_triangle(t_mywin *s_win);
-
-
-void	ft_get_polynome_lst(t_mywin *s_win);
-
-t_mytriangle	*ft_get_triangles_array(t_mywin *s_win);
-void	ft_display_triangle_array(t_mywin *s_win, t_mytriangle *triangle);
-
-
-float		ft_get_the_indice_vertex_x(int indice, t_myvec *vertex_lst);
-float		ft_get_the_indice_vertex_y(int indice, t_myvec *vertex_lst);
-float		ft_get_the_indice_vertex_z(int indice, t_myvec *vertex_lst);
-float	**ft_make_perspectiv(void);
-t_myvec		ft_scale_screen(t_myvec result);
-
-
-
-
-
-int	ft_triangle_clips_again_plan(t_myvec point, t_myvec plan, t_mytriangle *clipped_triangle, t_mytriangle *triangle, t_mywin *s_win);
-/* int		ft_calcul_culing(t_mychange *change, t_mytriangle *triangle); */
+/* t_myvec		ft_rotation_x(float angle, t_myvec vertex); */
+/* t_myvec		ft_rotation_y(float angle, t_myvec vertex); */
+/* t_myvec		ft_rotation_z(float angle, t_myvec vertex); */
+/* t_myvec		ft_matrix_multiply(float matrix[5][5], t_myvec vertex); */
+/* t_myvec		ft_translation_x(float value, t_myvec vertex); */
+/* t_myvec		ft_translation_y(float value, t_myvec vertex); */
+/* t_myvec		ft_translation_z(float value, t_myvec vertex); */
+/* t_myvec		ft_scale(float zoom, t_myvec vertex); */
+/* void			ft_draw_change(t_mywin *s_win, t_mychange *change); */
+/* t_myvec		ft_normalise(t_myvec vector); */
+//void			ft_mouse_event_check(t_win *wn, t_mychange *change, t_myraster *raster);
+/* void			ft_display_panel(); */
+/* float		**ft_make_projection(void);   //DONE */
+/* float		**ft_make_rotation_x(float theta);  //DONE */
+/* float		**ft_make_rotation_z(float theta); //DONE */
+/* float		**ft_matrix_multiply_matrix(float **m1, float **m2);  //surement DONE */
+/* float		**ft_make_matrix_5_5(void); */
+/* float		**ft_make_translation(float x, float y, float z); */
+/* t_myvec		ft_perspective_projection(t_myvec vertex); */
+/* void			ft_get_polynome_lst(t_mywin *s_win); */
+/* void			ft_display_triangle_array(t_mywin *s_win, t_mytriangle *triangle); */
+/* float		ft_get_the_indice_vertex_x(int indice, t_myvec *vertex_lst); */
+/* float		ft_get_the_indice_vertex_y(int indice, t_myvec *vertex_lst); */
+/* float		ft_get_the_indice_vertex_z(int indice, t_myvec *vertex_lst); */
+/* float		**ft_make_perspectiv(void); */
+/* t_myvec		ft_scale_screen(t_myvec result); */
+/* int			ft_triangle_clips_again_plan(t_myvec point, t_myvec plan, t_mytriangle *clipped_triangle, t_mytriangle *triangle, t_mywin *s_win); */
+/* int			ft_calcul_culing(t_mychange *change, t_mytriangle *triangle); */
 
 #endif
