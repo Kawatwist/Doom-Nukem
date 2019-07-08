1cfdf2c21eba265b7addb515c8cf5488796bf44c
Nabih ALI <naali@student.42.fr>
2019-07-08 06:32:50 +0200

    ajustement taille de text + choix de couleur

--- src/inventaire/srcs/inventaire.c
+++ src/inventaire/srcs/inventaire.c
@@ -6,7 +6,7 @@
 /*   By: naali <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/05 10:37:37 by naali             #+#    #+#             */
-/*   Updated: 2019/07/05 13:57:56 by naali            ###   ########.fr       */
+/*   Updated: 2019/07/08 06:31:29 by naali            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,22 +31,55 @@ static void		ft_init_inventaire_pos(t_win *wn, SDL_Rect *src, SDL_Rect *dst)
 	dst->h = wn->yscreen;// recuperer taille de fenetre
 }
 
+static void		ft_init_itemsname_pos(t_win *wn, SDL_Rect *dst, int pos, int size)
+{
+	dst->x = (wn->xscreen / 77);
+	dst->y = (wn->yscreen / 18) + (pos * 30);
+	dst->w = 10 * size;// recuperer taille de fenetre
+	dst->h = 25;// recuperer taille de fenetre
+}
+
+static void		choose_name_color(SDL_Color *color, int isselected)
+{
+	if (isselected == 1)
+	{
+		color->r = 0xFF;
+		color->g = 0x10;
+		color->b = 0x42;
+		color->a = 0xFF;
+	}
+	else
+	{
+		color->r = 0xFF;
+		color->g = 0x42;
+		color->b = 0xFF;
+		color->a = 0xFF;
+	}
+}
+
+static void		print_test_names(t_win *wn, char *name, int pos, int isselected)
+{
+	SDL_Rect	dst;
+	SDL_Color	color;
+
+	choose_name_color(&color, isselected);
+	ft_init_itemsname_pos(wn, &dst, pos, ft_strlen(name));
+	print_text_with_ariel_font(wn, name, color, dst);
+}
+
 void		print_inventory(t_win *wn)
 {
 	SDL_Rect	src;
 	SDL_Rect	dst;
-	SDL_Color	color;
 	SDL_Texture	*txt;
 
-	color.r = 0xFF;
-	color.g = 0x10;
-	color.b = 0x42;
-	color.a = 0xFF;
 	txt = findtexture(wn, "game", "inventaire", "bg");
 	ft_init_inventaire_pos(wn, &src, &dst);
 	SDL_SetTextureBlendMode(txt, (SDL_BLENDMODE_MOD));
 	SDL_SetRenderTarget(wn->rend, txt);
 	SDL_RenderCopy(wn->rend, txt, &src, &dst);
 	/* Print inventaire ICI */
-	print_text_with_ariel_font(wn, "test", color, src);//test
+	print_test_names(wn, "Salut", 0, 1);
+	print_test_names(wn, "Comment", 1, 0);
+	print_test_names(wn, "vas ?", 2, 0);
 }
