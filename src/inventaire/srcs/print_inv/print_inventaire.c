5fd6747945dda7504f87ffdf4e9f16484f332293
Nabih ALI <naali@student.42.fr>
2019-07-08 08:57:18 +0200

    Impression des armes et consomables sur la meme page OK, voir avec le groupe pour les modifications (suit)

---
+++ src/inventaire/srcs/print_inv/print_inventaire.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_inventaire.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: naali <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/08 07:32:26 by naali             #+#    #+#             */
+/*   Updated: 2019/07/08 08:21:10 by naali            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <doom.h>
+#include <inventaire.h>
+
+int				print_weapo_list(t_win *wn, t_joueurs *j, int *selected)
+{
+	int			i;
+	int			max;
+	t_weapons	*tmp;
+
+	i = 0;
+	tmp = j->inv->weapo;
+	max = weapo_list_len(&(j->inv->weapo));
+	while (i < max)
+	{
+		print_items_name(wn, tmp->name, i, ((*selected == i) ? 1 : 0));
+		print_items_qty(wn, ft_itoa(tmp->munition), i, ((*selected == i) ? 1 : 0));
+		tmp = tmp->next;
+		*selected = (*selected == i) ? -1 : *selected;
+		i++;
+	}
+	return (i);
+}
+
+void			print_conso_list(t_win *wn, t_joueurs *j, int selected, int pos)
+{
+	int				i;
+	int				max;
+	t_consumable	*tmp;
+
+	i = 0;
+	tmp = j->inv->conso;
+	max = conso_list_len(&(j->inv->conso));
+	selected -= pos;
+	while (i < max)
+	{
+		print_items_name(wn, tmp->name, pos, ((selected == i) ? 1 : 0));
+		print_items_qty(wn, ft_itoa(tmp->qty), pos, ((selected == i) ? 1 : 0));
+		tmp = tmp->next;
+		pos++;
+		i++;
+	}
+}
---
+++ src/inventaire/srcs/test_creation.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: naali <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/26 13:47:34 by naali             #+#    #+#             */
+/*   Updated: 2019/07/08 08:48:26 by naali            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "inventaire.h"
+
+static void		init_player(t_joueurs **j, char *name)
+{
+	if (((*j)->inv = (t_inventaire*)malloc(sizeof(t_inventaire))) == NULL)
+		exit(-1);
+	(*j)->inv->conso = NULL;
+	(*j)->inv->weapo = NULL;
+	(*j)->name = name;
+	(*j)->vie = 50;
+	(*j)->energie = 50;
+}
+
+int				test_joueur_creation(t_joueurs **j)
+{
+/* PLAYER */
+	if ((*j = (t_joueurs*)malloc(sizeof(t_joueurs))) == NULL)
+		return (-1);
+	init_player(j, "Bob");
+
+/* WEAPONS */
+	pushback_weapons(&((*j)->inv->weapo), new_weapon("Explosif", ZONE, 4));
+	pushback_weapons(&((*j)->inv->weapo), new_weapon("Lance", CAC, 1));
+	/* pushback_weapons(&((*j)->inv->weapo), new_weapon("Explosif", ZONE, 4)); */
+	/* pushback_weapons(&((*j)->inv->weapo), new_weapon("Lance", CAC, 1)); */
+	/* pushback_weapons(&((*j)->inv->weapo), new_weapon("Explosif", ZONE, 4)); */
+	/* pushback_weapons(&((*j)->inv->weapo), new_weapon("Lance", CAC, 1)); */
+/* 	if ((*j)->inv->weapo != NULL) */
+/* 	{ */
+/* 		printf("id = %d, name = %s, type = %d, dmg1 = %u\n", (*j)->inv->weapo->id, (*j)->inv->weapo->name, (*j)->inv->weapo->type, (*j)->inv->weapo->dmg[1]); */
+/* 		(*j)->inv->weapo->shoot(); */
+/* 		(*j)->inv->weapo->previous->shoot(); */
+/* 		(*j)->inv->weapo->previous->previous->shoot(); */
+/* 		(*j)->inv->weapo->next->next->shoot(); */
+/* 	} */
+
+/* CONSUMABLE */
+	pushback_conso(&((*j)->inv->conso), new_conso("potion", 3, &((*j)->inv->conso)));
+	pushback_conso(&((*j)->inv->conso), new_conso("potion", 3, &((*j)->inv->conso)));
+	pushback_conso(&((*j)->inv->conso), new_conso("potion", 3, &((*j)->inv->conso)));
+	pushback_conso(&((*j)->inv->conso), new_conso("potion", 3, &((*j)->inv->conso)));
+	pushback_conso(&((*j)->inv->conso), new_conso("potionV2", 3, &((*j)->inv->conso)));
+	pushback_conso(&((*j)->inv->conso), new_conso("batterie", 5, &((*j)->inv->conso)));
+	pushback_conso(&((*j)->inv->conso), new_conso("batterieV2", 5, &((*j)->inv->conso)));
+/* 	printf("qty = %d, used = %d\n", (*j)->inv->conso->qty, (*j)->inv->conso->used); */
+/* 	(*j)->inv->conso->use(&j, (*j)->inv->conso); */
+/* 	(*j)->inv->conso->use(&j, (*j)->inv->conso); */
+/* 	(*j)->inv->conso->use(&j, (*j)->inv->conso); */
+/* 	(*j)->inv->conso->use(&j, (*j)->inv->conso); */
+/* 	(*j)->inv->conso->use(&j, (*j)->inv->conso); */
+/* 	(*j)->inv->conso->use(&j, (*j)->inv->conso); */
+/* 	(*j)->inv->conso->next->use(&j, (*j)->inv->conso->next); */
+/* 	(*j)->inv->conso->previous->use(&j, (*j)->inv->conso->previous); */
+
+/* 	printf("NB conso = %d\n", conso_list_len(&((*j)->inv->conso))); */
+/* 	conso_swap_elem(&((*j)->inv->conso), 2, 4); */
+/* 	conso_swap_elem(&((*j)->inv->conso), 3, 4); */
+/* 	destroy_one_conso(&((*j)->inv->conso), 1); */
+/* 	printf("NB conso = %d\n", conso_list_len(&((*j)->inv->conso))); */
+/* 	printf("NB weapo = %d\n", weapo_list_len(&((*j)->inv->weapo))); */
+/* 	destroy_conso_list(&((*j)->inv->conso)); */
+/* 	destroy_weapo_list(&((*j)->inv->weapo)); */
+	return (0);
+}
