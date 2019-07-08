2c618be3a737ca7a129190cccb91a85c0f16df91
jchardin <jerome.chardin@outook.com>
2019-07-08 22:07:03 +0200

    marche

--- src/jeronemo/dir_game_engine/file_game_engine.c
+++ src/jeronemo/dir_game_engine/file_game_engine.c
@@ -6,7 +6,7 @@
 /*   By: jchardin <jerome.chardin@outlook.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 11:45:42 by jchardin          #+#    #+#             */
-/*   Updated: 2019/07/08 22:04:20 by jchardin         ###   ########.fr       */
+/*   Updated: 2019/07/08 22:06:48 by jchardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,10 @@ void	ft_launch_rasterization(t_mywin *s_win, t_win *wn)
 	ft_launch_bsp_tree(s_win, polygon_lst);
 	//s_win->polygon_lst = polygon_lst;
 	triangle_array = ft_get_triangles_array(s_win);
-	int max = ft_get_nbr_of_triangle(s_win->polygon_lst);
+	polygon_lst = s_win->polygon_lst;
+	int max = ft_get_nbr_of_triangle(polygon_lst);
+
+
 	ft_init_rasterization(wn, &raster);
 	/* ft_display_triangle_array(s_win, triangle_array, max); */
 	while (!raster.quit)
