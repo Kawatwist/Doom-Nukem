602fc502093dc196a46c91ae68b921e96ea9617c
Jsauron <jsauron@student.42.fr>
2019-08-19 23:17:34 +0200

    get_intersection

--- src/jeronemo/dir_bsp/file_split_polygon.c
+++ src/jeronemo/dir_bsp/file_split_polygon.c
@@ -6,7 +6,7 @@
 /*   By: jchardin <jerome.chardin@outlook.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 16:20:59 by jchardin          #+#    #+#             */
-/*   Updated: 2019/07/08 12:59:24 by jchardin         ###   ########.fr       */
+/*   Updated: 2019/08/19 23:17:20 by jsauron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,54 +25,64 @@ int			ft_get_intersect(t_myvec *line_start,
 	float		line_length;
 	float		dist_from_plane;
 
-	//[...]
+	direction.x = line_end->x - line_start->x;
+	direction.y = line_end->y - line_start->y;
+	direction.z = line_end->z - line_start->z;
 
-	return (0);
+	line_length = ft_dot_product(direction, normal);
+
+	if (fabsf(line_length) < 0.0001)
+		return 0;
+
+	L1.x = vertex->x - line_start->x;
+	L1.y = vertex->y - line_start->y;
+	L1.z = vertex->z - line_start->z;
+
+	dist_from_plane = ft_dot_product(L1, normal);
+	percentage = dist_from_plane / line_length;
+
+	if (percentage<0.0f)
+			return (0);
+	else if (percentage > 1.0f)
+			return (0);
+
+	intersection->x = line_start->x + direction.x * (percentage);
+	intersection->y = line_start->y + direction.y * (percentage);
+	intersection->z = line_start->z + direction.z * (percentage);
+	return (1);
 }
 
 
 
-void		ft_split_polygon(t_mypolygon *poly,
-							t_mypolygon *plane,
-							t_mypolygon *front_split,
-							t_mypolygon *back_split)
+void		ft_split_polygon(t_mypolygon *poly, t_mypolygon *plane, t_mypolygon *front_split, t_mypolygon *back_split)
 {
 	t_myvec		front_list[20];
 	t_myvec		back_list[20];
-	t_myvec		*first_vertex;
+	t_myvec		first_vertex;
 	t_myvec		plane_normal;
 	t_myvec		intersect_point;
-	t_myvec		*point_on_plane;
+	t_myvec		point_on_plane;
 	t_myvec		point_a;
 	t_myvec		point_b;
 	int			front_counter = 0;
 	int			back_counter = 0;
-	int			i;
+	int			i = 0;
 	int			current_vertex;
 	float		percent;
 
 
-	point_on_plane = plane->vertex_lst;  //un point fesant partis du polygon splitter
-	first_vertex = poly->vertex_lst;
-
+	point_on_plane = plane->vertex_lst[0]t;  //un point fesant partis du polygon splitter
+	first_vertex = poly->vertex_lst[0];
 	int result;
-	result = ft_classify_point(*first_vertex, plane);
+	result = ft_classify_point(first_vertex, plane);
 	if (result == FRONT)
-	{
-		front_list[front_counter] = *first_vertex;
-		front_counter++;
-	}
+		front_list[front_counter++] = first_vertex;
 	else if (result == BACK)
-	{
-		back_list[back_counter] = *first_vertex;
-		back_counter++;
-	}
+		back_list[back_counter++] = first_vertex;
 	else if (result == ON_PLANE)
 	{
-		front_list[front_counter] = *first_vertex;
-		front_counter++;
-		back_list[back_counter] = *first_vertex;
-		back_counter++;
+		front_list[front_counter++] = first_vertex;
+		back_list[back_counter++] = first_vertex;
 	}
 	////////////////////////////////////
 	i = 1;
@@ -91,30 +101,23 @@ void		ft_split_polygon(t_mypolygon *poly,
 
 		if (result == ON_PLANE) //// on ajoute au deux list
 		{
-			front_list[front_counter] = *first_vertex;
-			front_counter++;
-			back_list[back_counter] = *first_vertex;
-			back_counter++;
+				front_list[front_counter++] = poly->vertex_list[current_vertex];
+				back_list[back_counter++] = poly->vertex_list[current_vertex];
 		}
 		else
 		{
-			if (ft_get_intersect())
+			if (ft_get_intersect(point_a, point_b, point_on_plane, plane_normal, intersect_point, percent))
 			{
+
 				/////si il y intersection if a edge get intersect
 			}
 			else
 			{
 				///// si il il n'y a pas d'intersection
 				if (result == FRONT && current_vertex != 0)
-				{
-					front_list[front_counter] = poly->vertex_list[current_vertex]; ///// a changer
-					front_counter++;
-				}
+					front_list[front_counter++] = poly->vertex_list[current_vertex]; ///// a changer
 				if (result == BACK && curent_vertex != 0)
-				{
-					back_list[front_counter] = poly->vertex_list[current_vertex]; ///// a changer
-					back_counter++;
-				}
+					back_list[back_counter++] = poly->vertex_list[current_vertex]; ///// a changer
 			}
 		}
 		i++;
