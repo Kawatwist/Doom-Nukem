dd416e6f485b4dd8f206fd4bd7e665e15d4af533
Kawatwist <masse.loic2@hotmail.com>
2019-10-14 10:53:35 +0200

    edit

--- src/editor/mainedit.c
+++ src/editor/mainedit.c
@@ -21,8 +21,8 @@ void		edit(t_win *wn)
 	}
 	inputeditor(wn);
 	printeditor(wn);
-	SDL_Rect	pos = define_rect(((t_edit *)wn->edit)->tab->bg.x, ((t_edit*)wn->edit)->tab->bg.y + 200, ((t_edit*)wn->edit)->tab->bg.w - 50, ((t_edit*)wn->edit)->tab->bg.h);	
-	SDL_Rect	check = define_rect(wn->input->x, wn->input->y, 0, 0);
+	// SDL_Rect	pos = define_rect(((t_edit *)wn->edit)->tab->bg.x, ((t_edit*)wn->edit)->tab->bg.y + 200, ((t_edit*)wn->edit)->tab->bg.w - 50, ((t_edit*)wn->edit)->tab->bg.h);	
+	// SDL_Rect	check = define_rect(wn->input->x, wn->input->y, 0, 0);
 	// if (boxhitbox(wn->rend, &check, &pos, 1))
 	// 	save_panel(wn, wn->edit);
 }
